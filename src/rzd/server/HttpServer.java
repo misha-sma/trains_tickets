4b573e1cfee23ee339132242af9cf47a63f14d09
misha-sma <misha-sma@yandex.ru>
2019-01-19 03:18:21 +0300

    add calendar

--- src/rzd/server/HttpServer.java
+++ src/rzd/server/HttpServer.java
@@ -20,179 +20,190 @@ import rzd.persistence.entity.Train;
 import rzd.util.Util;
 
 public class HttpServer {
-  public static final String HOME_PAGE;
-  public static final String HOME_PAGE_BEGIN;
-  public static final String HOME_PAGE_END;
-  public static byte[] faviconBytes;
+	public static final String HOME_PAGE;
+	public static final String HOME_PAGE_BEGIN;
+	public static final String HOME_PAGE_END;
+	public static byte[] faviconBytes;
 
-  public static int HITS_COUNT = 10;
+	static {
+		HOME_PAGE = Util.loadText("web/index.html");
+		int endBodyIndex = HOME_PAGE.indexOf("</body>");
+		HOME_PAGE_BEGIN = HOME_PAGE.substring(0, endBodyIndex);
+		HOME_PAGE_END = HOME_PAGE.substring(endBodyIndex);
+		File file = new File("web/favicon.ico");
+		try {
+			FileInputStream input = new FileInputStream(file);
+			faviconBytes = new byte[(int) file.length()];
+			input.read(faviconBytes);
+			input.close();
+		} catch (FileNotFoundException e) {
+			// logger.error(e);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// logger.error(e);
+			e.printStackTrace();
+		}
+	}
 
-  static {
-      HOME_PAGE = Util.loadText("web/index.html");
-      int endBodyIndex = HOME_PAGE.indexOf("</body>");
-      HOME_PAGE_BEGIN = HOME_PAGE.substring(0, endBodyIndex);
-      HOME_PAGE_END = HOME_PAGE.substring(endBodyIndex);
-      File file = new File("web/favicon.ico");
-      try {
-          FileInputStream input = new FileInputStream(file);
-          faviconBytes = new byte[(int) file.length()];
-          input.read(faviconBytes);
-          input.close();
-      } catch (FileNotFoundException e) {
-//          logger.error(e);
-        e.printStackTrace();
-      } catch (IOException e) {
-//          logger.error(e);
-        e.printStackTrace();
-      }
-  }
+	private static class SocketProcessor implements Runnable {
+		private Socket socket;
+		private InputStream is;
+		private OutputStream os;
 
-  private static class SocketProcessor implements Runnable {
-      private Socket socket;
-      private InputStream is;
-      private OutputStream os;
+		private SocketProcessor(Socket socket) throws Throwable {
+			this.socket = socket;
+			this.is = socket.getInputStream();
+			this.os = socket.getOutputStream();
+		}
 
-      private SocketProcessor(Socket socket) throws Throwable {
-          this.socket = socket;
-          this.is = socket.getInputStream();
-          this.os = socket.getOutputStream();
-      }
+		public void run() {
+			try {
+				String headers = readInputHeaders();
+				String url = getUrl(headers);
+				if (url.equals("favicon.ico")) {
+					writeFaviconResponse();
+				} else if (url.startsWith("?from=")) {
+					try {
+						url = URLDecoder.decode(url, "UTF8");
+					} catch (UnsupportedEncodingException e) {
+						// logger.error(e);
+						e.printStackTrace();
+					}
+					System.out.println("url=" + url);
+					Map<String, String> params = Util.parseParameters(url);
+					String departureStation = params.get("from");
+					String destinationStation = params.get("to");
+					int idDepartureStation = TrainDao.getIdStation(departureStation);
+					int idDestinationStation = TrainDao.getIdStation(destinationStation);
+					String date = params.get("date");
+					StringBuilder builder = new StringBuilder();
+					String header = HOME_PAGE_BEGIN.replace("placeholder=\"Откуда\"",
+							"value=\"" + departureStation + "\"");
+					header = header.replace("placeholder=\"Куда\"", "value=\"" + destinationStation + "\"");
+					header = header.replace("placeholder=\"Когда\"", "value=\"" + date + "\"");
+					builder.append(header);
+					builder.append("<table border=\"1\">\n");
+					builder.append(
+							"<tr>\n<th>Поезд</th>\n<th>Время отправления</th>\n<th>Время в пути</th>\n<th>Время прибытия</th>\n");
+					boolean isAllDays = date == null || date.isEmpty();
+					if (isAllDays) {
+						builder.append("<th>Выбрать дату</th>\n</tr>\n");
+					} else {
+						builder.append("<th></th>\n</tr>\n");
+					}
+					List<Integer> trains = null;
+					if (isAllDays) {
+						trains = TrainDao.getTrainsByStations(idDepartureStation, idDestinationStation);
+					} else {
+						trains = TrainDao.getTrainsByStationsAndDate(idDepartureStation, idDestinationStation, date);
+					}
 
-      public void run() {
-          try {
-              String headers = readInputHeaders();
-              String url = getUrl(headers);
-              if (url.equals("favicon.ico")) {
-                  writeFaviconResponse();
-              } else if (url.startsWith("?from=")) {
-                try {
-                  url = URLDecoder.decode(url, "UTF8");
-              } catch (UnsupportedEncodingException e) {
-                  //logger.error(e);
-                e.printStackTrace();
-              }
-          System.out.println("url=" + url);
-          Map<String, String> params = Util.parseParameters(url);
-          String departureStation = params.get("from");
-          String destinationStation = params.get("to");
-          int idDepartureStation = TrainDao.getIdStation(departureStation);
-          int idDestinationStation = TrainDao.getIdStation(destinationStation);
-          String date = params.get("date");
-          StringBuilder builder = new StringBuilder();
-          String header = HOME_PAGE_BEGIN.replace("placeholder=\"Откуда\"", "value=\"" + departureStation + "\"");
-          header = header.replace("placeholder=\"Куда\"", "value=\"" + destinationStation + "\"");
-          header = header.replace("placeholder=\"Когда\"", "value=\"" + date + "\"");
-          builder.append(header);
-          builder.append("<table border=\"1\">\n");
-          builder.append(
-              "<tr>\n<th>Поезд</th>\n<th>Время отправления</th>\n<th>Время в пути</th>\n<th>Время прибытия</th>\n<th></th>\n</tr>\n");
-          List<Integer> trains = null;
-          boolean isAllDays=date == null || date.isEmpty();
-          if (isAllDays) {
-            trains = TrainDao.getTrainsByStations(idDepartureStation, idDestinationStation);
-          } else {
-            trains = TrainDao.getTrainsByStationsAndDate(idDepartureStation, idDestinationStation, date);
-          }
+					for (int idTrain : trains) {
+						String trainDepartureStation = TrainDao.getDepartureStation(idTrain);
+						String trainDestinationStation = TrainDao.getDestinationStation(idTrain);
+						Train train = TrainDao.getTrainById(idTrain);
+						int departureTravelTime = TrainDao.getTravelStayTime(idTrain, idDepartureStation);
+						int destinationTravelTime = TrainDao.getTravelTime(idTrain, idDestinationStation);
+						String depTime = Util.addMinutesToDate(train.getDepartureTime(), departureTravelTime);
+						String destTime = Util.addMinutesToDate(train.getDepartureTime(), destinationTravelTime);
+						builder.append("<tr><td>").append(
+								train.getIdTrain() + " " + trainDepartureStation + " - " + trainDestinationStation);
+						if (train.getName() != null && !train.getName().isEmpty()) {
+							builder.append("<br>" + train.getName());
+						}
+						builder.append("</td>\n<td>" + depTime + "</td>\n<td>"
+								+ Util.formatMinutes(destinationTravelTime - departureTravelTime) + "</td>\n<td>"
+								+ destTime + "</td>\n");
+						if (isAllDays) {
+							builder.append(
+									"<td>\n<form method='get' name='selectDate'>\n<input type=\"date\" name=\"date\">\n"
+											+ "<input type=\"hidden\" name=\"idTrain\" value=\"" + idTrain + "\" />"
+											+ "<input type=\"hidden\" name=\"from\" value=\"" + idDepartureStation
+											+ "\" />" + "<input type=\"hidden\" name=\"to\" value=\""
+											+ idDestinationStation + "\" />"
+											+ "<input type=\"submit\" name=\"submitDateButton\" value=\"Выбрать\">");
+						} else {
+							builder.append(
+									"<td>\n<form method='get' name='selectSeat'>\n<input type=\"submit\" name=\"selectSeatButton\" value=\"Выбрать место\">\n");
+						}
+						builder.append("</form>\n" + train.getDepartureDays() + "</td>\n</tr>\n");
+					}
+					builder.append("</table>\n");
+					builder.append(HOME_PAGE_END);
+					writeHomePageResponse(builder.toString());
+				} else {
+					writeHomePageResponse(HOME_PAGE);
+				}
+			} catch (Throwable t) {
+				// logger.error(t);
+				t.printStackTrace();
+			} finally {
+				try {
+					socket.close();
+				} catch (Throwable t) {
+					// logger.error(t);
+					t.printStackTrace();
+				}
+			}
+			// logger.info("----------------------Client processing
+			// finished---------------------------");
+			System.out.println("----------------------Client processing finished---------------------------");
+		}
 
-          for (int idTrain : trains) {
-            String trainDepartureStation = TrainDao.getDepartureStation(idTrain);
-            String trainDestinationStation = TrainDao.getDestinationStation(idTrain);
-            Train train = TrainDao.getTrainById(idTrain);
-            int departureTravelTime = TrainDao.getTravelStayTime(idTrain, idDepartureStation);
-            int destinationTravelTime = TrainDao.getTravelTime(idTrain, idDestinationStation);
-            String depTime = Util.addMinutesToDate(train.getDepartureTime(), departureTravelTime);
-            String destTime = Util.addMinutesToDate(train.getDepartureTime(), destinationTravelTime);
-            builder.append("<tr><td>")
-                .append(train.getIdTrain() + " " + trainDepartureStation + " - " + trainDestinationStation);
-            if (train.getName() != null && !train.getName().isEmpty()) {
-              builder.append("<br>" + train.getName());
-            }
-            builder.append("</td>\n<td>" + depTime + "</td>\n<td>"
-                + Util.formatMinutes(destinationTravelTime - departureTravelTime) + "</td>\n<td>" + destTime
-                + "</td>\n");
-            if (isAllDays) {
-              builder.append(
-                  "<td>\n<form method='get' name='selectDate'>\n<input type=\"submit\" name=\"selectDateButton\" value=\"Выбрать дату\">\n");
-            } else {
-              builder.append(
-                  "<td>\n<form method='get' name='selectSeat'>\n<input type=\"submit\" name=\"selectSeatButton\" value=\"Выбрать место\">\n");
-            }
-            builder.append("</form>\n" + train.getDepartureDays() + "</td>\n</tr>\n");
-          }
-          builder.append("</table>\n");
-          builder.append(HOME_PAGE_END);
-          writeHomePageResponse(builder.toString());
-        } else {
-          writeHomePageResponse(HOME_PAGE);
-        }
-          } catch (Throwable t) {
-//              logger.error(t);
-            t.printStackTrace();
-          } finally {
-              try {
-                  socket.close();
-              } catch (Throwable t) {
-//                  logger.error(t);
-                t.printStackTrace();
-              }
-          }
-//          logger.info("----------------------Client processing finished---------------------------");
-      System.out.println("----------------------Client processing finished---------------------------");
-      }
+		private void writeHomePageResponse(String html) throws Throwable {
+			String response = "HTTP/1.1 200 OK\r\n" + "Server: misha-sma-Server/2012\r\n"
+					+ "Content-Type: text/html\r\n" + "Set-Cookie: SSSAAAA=GGGDDDDD\r\n" + "cookie: SSS=GGG;path=/\r\n"
+					+ "Connection: close\r\n\r\n";
+			String result = response + html;
+			os.write(result.getBytes());
+			os.flush();
+		}
 
-      private void writeHomePageResponse(String html) throws Throwable {
-          String response = "HTTP/1.1 200 OK\r\n" + "Server: misha-sma-Server/2012\r\n"
-                  + "Content-Type: text/html\r\n" + "Connection: close\r\n\r\n";
-          String result = response + html;
-          os.write(result.getBytes());
-          os.flush();
-      }
+		private void writeFaviconResponse() throws Throwable {
+			String response = "HTTP/1.1 200 OK\r\n" + "Server: misha-sma-Server/2012\r\n"
+					+ "Content-Type: image/vnd.microsoft.icon\r\n" + "Content-Length: " + faviconBytes.length + "\r\n"
+					+ "Connection: close\r\n\r\n";
+			os.write(response.getBytes());
+			os.write(faviconBytes);
+			os.flush();
+		}
 
-      private void writeFaviconResponse() throws Throwable {
-          String response = "HTTP/1.1 200 OK\r\n" + "Server: misha-sma-Server/2012\r\n"
-                  + "Content-Type: image/vnd.microsoft.icon\r\n" + "Content-Length: " + faviconBytes.length + "\r\n"
-                  + "Connection: close\r\n\r\n";
-          os.write(response.getBytes());
-          os.write(faviconBytes);
-          os.flush();
-      }
+		private String readInputHeaders() throws Throwable {
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			StringBuilder builder = new StringBuilder();
+			String line;
+			while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
+				// logger.debug("line=" + line);
+				System.out.println("line=" + line);
+				builder.append(line).append('\n');
+			}
+			return builder.toString();
+		}
+	}
 
-      private String readInputHeaders() throws Throwable {
-          BufferedReader br = new BufferedReader(new InputStreamReader(is));
-          StringBuilder builder = new StringBuilder();
-          String line;
-          while ((line = br.readLine()) != null && !line.trim().isEmpty()) {
-//              logger.debug("line=" + line);
-            System.out.println("line=" + line);  
-            builder.append(line).append('\n');
-          }
-          return builder.toString();
-      }
-  }
+	public static void main(String[] args) throws Throwable {
+		ServerSocket serverSocket = new ServerSocket(8080);
+		// logger.info("Server started!!!");
+		System.out.println("Server started!!!");
+		while (true) {
+			Socket socket = serverSocket.accept();
+			// logger.info("---------------Client accepted------------------------");
+			System.out.println("---------------Client accepted------------------------");
+			new Thread(new SocketProcessor(socket)).start();
+		}
+	}
 
-  public static void main(String[] args) throws Throwable {
-      ServerSocket serverSocket = new ServerSocket(8080);
-      //logger.info("Server started!!!");
-      System.out.println("Server started!!!");
-      while (true) {
-          Socket socket = serverSocket.accept();
-          //logger.info("---------------Client accepted------------------------");
-          System.out.println("---------------Client accepted------------------------");
-          new Thread(new SocketProcessor(socket)).start();
-      }
-  }
-
-  public static String getUrl(String headers) {
-      String url = "";
-      StringTokenizer tokenizer = new StringTokenizer(headers);
-      while (tokenizer.hasMoreTokens()) {
-          String word = tokenizer.nextToken();
-          if (word.equals("GET")) {
-              url = tokenizer.nextToken();
-          }
-      }
-      url = url.startsWith("/") ? url.substring(1) : url;
-      url = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
-      return url;
-  }
+	public static String getUrl(String headers) {
+		String url = "";
+		StringTokenizer tokenizer = new StringTokenizer(headers);
+		while (tokenizer.hasMoreTokens()) {
+			String word = tokenizer.nextToken();
+			if (word.equals("GET")) {
+				url = tokenizer.nextToken();
+			}
+		}
+		url = url.startsWith("/") ? url.substring(1) : url;
+		url = url.endsWith("/") ? url.substring(0, url.length() - 1) : url;
+		return url;
+	}
 }
